b3008d84d27e8d8b42ddb9ba441e5b92e7688c22
A <941636007@qq.com>
2017-05-25 17:54:13 +0800

    Macro

---
+++ Example/JKBaseKit/JKMacro.h
@@ -0,0 +1,15 @@
+//
+//  JKMacro.h
+//  JKBaseKit
+//
+//  Created by A on 2017/5/25.
+//  Copyright © 2017年 liuweiqiang. All rights reserved.
+//
+
+#ifndef JKMacro_h
+#define JKMacro_h
+
+
+
+
+#endif /* JKMacro_h */
