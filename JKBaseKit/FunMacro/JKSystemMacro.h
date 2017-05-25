e00a162ef1c9cccf73e141a996579ebe951bc919
A <941636007@qq.com>
2017-05-25 18:49:00 +0800

    统一宏定义

--- JKBaseKit/FunMacro/JKSystemMacro.h
+++ JKBaseKit/FunMacro/JKSystemMacro.h
@@ -11,21 +11,6 @@
 
 #import "JKBaseKit.h"
 
-/** 当前系统版本 */
-#define CurrentSystem                           [[[UIDevice currentDevice] systemVersion] floatValue]
-/** 获取APP的版本号 */
-#define kSoftwareVersion                        ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"])
-/** 获取app内部的版本号 */
-#define kBuildwareVersion                       ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"])
-/**获取app的名称**/
-#define kBundleDisplayName                      ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"])
-
-
-#ifdef DEBUG
-    #define NSLog(...) NSLog(__VA_ARGS__) //测试环境输出
-#else
-    #define NSLog(...)
-#endif
 
 
 #endif /* SystemMacro_h */
--- JKBaseKit/JKMacro.h
+++ JKBaseKit/JKMacro.h
@@ -9,12 +9,25 @@
 #ifndef JKMacro_h
 #define JKMacro_h
 
-/// UI Macro
-#define jkScreenWidth        ([UIScreen mainScreen].bounds).size.width
 
-#define jkScreenHeight       ([UIScreen mainScreen].bounds).size.height
+/// NSBundle
+/** 当前系统版本 */
+#define jkCurrentSystem                          [[[UIDevice currentDevice] systemVersion] floatValue]
+/** 获取APP的版本号 */
+#define jkSoftwareVersion                        ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleShortVersionString"])
+/** 获取app内部的版本号 */
+#define jkBuildwareVersion                       ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleVersion"])
+/**获取app的名称**/
+#define jkBundleDisplayName                      ([[NSBundle mainBundle] objectForInfoDictionaryKey:@"CFBundleDisplayName"])
 
-#define jkStatusBarHeight    [UIApplication sharedApplication].statusBarFrame.size.height
+
+/// NSLog
+#ifdef DEBUG
+#define NSLog(...) NSLog(__VA_ARGS__) //测试环境输出
+#else
+#define NSLog(...)
+#endif
+
 
 /// Rect
 #define jkRect(origin, w, h) ((CGRect){.origin = origin, .size = { w, h}})
