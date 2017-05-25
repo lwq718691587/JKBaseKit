ed2478afb3174bbbad9f6c7c460d36c9ca668db1
刘伟强 <liuweiqiang@jianke.com>
2017-05-25 16:46:09 +0800

    修改podspec文件

--- JKBaseKit/JKBaseKit.h
+++ JKBaseKit/JKBaseKit.h
@@ -10,17 +10,11 @@
 #define JKBaseKit_h
 
 
-#import "Aspects.h"
 #import "UIButton+WebCache.h"
 #import "UIImageView+WebCache.h"
-#import "MJRefresh.h"
-#import "DateTools.h"
-#import "IQKeyboardManager.h"
 #import "YYModel.h"
-#import "UIView+Additional.h"
 #import "JKSystemMacro.h"
 #import "NSObject+StareValeu.h"
-#import "UIColor+HEX.h"
-#import "UIImage+Addition.h"
+
 
 #endif /* JKBaseKit_h */
